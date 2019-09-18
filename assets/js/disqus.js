5a2f802990fd883a44e6d466881c8da5f6d9f56d
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-09-18 22:56:37 +0900

    Add Disqus comment

---
+++ assets/js/disqus.js
@@ -0,0 +1,6 @@
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://who-am-i-1.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
