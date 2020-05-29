b68a98a6f03bc5f0f34c871401a8cd7f0e5d6264
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-05-29 16:03:53 +0900

    Create reset.scss
    Install node-sass
    Set font famaly to Ubuntu

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
+import './scss/reset.scss';
 
 ReactDOM.render(
   <React.StrictMode>
