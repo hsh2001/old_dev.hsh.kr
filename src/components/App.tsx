2f70055d961e4c94b02d57cb73bc9e34d9a2cc42
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-05-31 06:43:34 +0900

    Update for lazy loading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,76 @@
-import React from 'react';
-import IntroducePage from './IntroducePage';
-import BlinkView from './BlinkView';
-import ReversChangePage from './ReversChangePage';
-import AIOmokPage from './AIOmokPage';
-import CRIPage from './CRIPage';
-import TypoChangePage from './TypoChangePage';
-import ContactPage from './ContactPage';
+import React, { useState, useEffect } from 'react';
+import IntroducePage, { profileImageLoad } from './IntroducePage';
 
 const App: React.FC = () => {
+  const [message, setMessage] = useState('Loading... Please wait.');
+  const [mainContent, setmainContent] = useState(<></>);
+
+  useEffect(() => {
+    profileImageLoad.then(async () => {
+      let loadCount = 0;
+      const moduleLoads = [
+        import('./BlinkView'),
+        import('./ReversChangePage'),
+        import('./AIOmokPage'),
+        import('./CRIPage'),
+        import('./TypoChangePage'),
+        import('./ContactPage'),
+      ] as const;
+
+      function displayLoadingCount(): void {
+        setMessage(`Loading components... ${loadCount}/${moduleLoads.length}`);
+      }
+
+      displayLoadingCount();
+
+      moduleLoads.forEach((load) => {
+        Promise.all([load]).then(() => {
+          loadCount++;
+          displayLoadingCount();
+        });
+      });
+
+      const [
+        { default: BlinkView },
+        { default: ReversChangePage },
+        { default: AIOmokPage },
+        { default: CRIPage, frames },
+        { default: TypoChangePage },
+        { default: ContactPage },
+      ] = await Promise.all(moduleLoads);
+
+      loadCount = 0;
+      setMessage(`Loading images... ${loadCount}/${frames.length}`);
+      await Promise.all(
+        frames.map((frame) => {
+          return frame.promise.then(() => {
+            loadCount++;
+            setMessage(`Loading images... ${loadCount}/${frames.length}`);
+          });
+        }),
+      );
+
+      setmainContent(
+        <>
+          <BlinkView title="I am a Game Developer." />
+          <ReversChangePage />
+          <AIOmokPage />
+          <BlinkView title="I am a Web Developer." />
+          <CRIPage />
+          <TypoChangePage />
+          <hr />
+          <ContactPage />
+        </>,
+      );
+
+      setMessage(`The loading is done! Scroll down for more information.`);
+    });
+  }, []);
+
   return (
     <>
-      <IntroducePage />
-      <BlinkView title="I am a Game Developer." />
-      <ReversChangePage />
-      <AIOmokPage />
-      <BlinkView title="I am a Web Developer." />
-      <CRIPage />
-      <TypoChangePage />
-      <hr />
-      <ContactPage />
+      <IntroducePage message={message} />
+      {mainContent}
     </>
   );
 };
