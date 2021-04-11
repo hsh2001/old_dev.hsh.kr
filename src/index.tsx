138b34994d3ed6cfaf6a248ceedc91ce5fd1cc47
Seung-hyun Hwang <dev@hsh.kr>
2021-04-12 02:02:27 +0900

    Remove buy-me-a-coffee.scss

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import Root from './components/Root';
 import './scss/reset.scss';
-import './scss/buy-me-a-coffee.scss';
 import './scss/facebook.scss';
 
 ReactDOM.render(<Root />, document.getElementById('root'));
